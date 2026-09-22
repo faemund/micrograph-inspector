e698cf89233996cf3bc08783eb237e3fde0b514c
Gerrit Timpert <gerrit.timpert@scilifelab.se>
2026-09-22 13:41:53 +0200

    Improved style and fixes

--- micrograph-inspector.py
+++ micrograph-inspector.py
@@ -1,16 +1,29 @@
+from pathlib import Path
 import mrcfile
-with mrcfile.open('data/test.mrc') as mrc:
-    
+import sys
+
+p = Path(sys.argv[1])
+
+with mrcfile.open(p) as mrc:
+
+    print()
+    print('Micrograph Inspection')
+    print('---------------------')
+    print()
+    print('File: ', p.name)
+    print()
+
     dim = len(mrc.data.shape)
     if dim == 3:
         y = mrc.data.shape[1]
         x = mrc.data.shape[2]
-    elif dim == 3:
+    elif dim == 2:
         y = mrc.data.shape[0]
         x = mrc.data.shape[1]
 
     print('Height: ', y)
     print('Width: ', x)
+    print()
 
     if mrc.is_volume():
         print('File represents: volume.')
@@ -23,8 +36,11 @@ with mrcfile.open('data/test.mrc') as mrc:
     else:
         print('Could not analyze what this file represents')
 
+    print()
     print('Voxel size:')
+    print()
     print('x =', mrc.voxel_size.x, ' Å/px')
     print('y =', mrc.voxel_size.y, ' Å/px')
     print('z =', mrc.voxel_size.z, ' Å/px')
+    print()
     
