ff5f0849f56214b9a624786f4fe8d7dbf82344b9
Gerrit Timpert <gerrit.timpert@scilifelab.se>
2026-09-22 13:29:16 +0200

    Implemented file type, voxel, voxel size

--- micrograph-inspector.py
+++ micrograph-inspector.py
@@ -5,11 +5,26 @@ with mrcfile.open('data/test.mrc') as mrc:
     if dim == 3:
         y = mrc.data.shape[1]
         x = mrc.data.shape[2]
-
-    else:
+    elif dim == 3:
         y = mrc.data.shape[0]
         x = mrc.data.shape[1]
 
     print('Height: ', y)
     print('Width: ', x)
+
+    if mrc.is_volume():
+        print('File represents: volume.')
+    elif mrc.is_image_stack():
+        print('File represents: image stack.')
+    elif mrc.is_single_image():
+        print('File represents: single image.')
+    elif mrc.is_volume_stack():
+        print('File represents: volume stack.')
+    else:
+        print('Could not analyze what this file represents')
+
+    print('Voxel size:')
+    print('x =', mrc.voxel_size.x, ' Å/px')
+    print('y =', mrc.voxel_size.y, ' Å/px')
+    print('z =', mrc.voxel_size.z, ' Å/px')
     
